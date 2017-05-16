2028153672968b1524a878cd32cce648e887188f
AynmShawn4 <x33tang@uwaterloo.ca>
2017-05-15 22:25:45 -0400

    Add files via upload

---
+++ Qaudris/Main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: 2017Äê5ÔÂ14ÈÕ
+ *      Author: ShawnTang
+ */
+
+#include "GameManager.h"
+
+int main(){
+
+	GameManager * gm = new GameManager();
+
+	gm->start();
+
+	return 0;
+
+}
